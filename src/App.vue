3807f05222390149e29a41ea70e12edf22829064
Glennn <nicholas.glenn@safholland.com>
2018-11-06 11:02:00 -0500

    Initial base files and changes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/tech">Tech</router-link> |
+      <router-link to="/entertainment">Entertainment</router-link> |
+      <router-link to="/weather">Weather</router-link> |
+
     </div>
     <router-view/>
   </div>
@@ -26,4 +30,24 @@
     }
   }
 }
+
+.container{
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.row{
+// rows for the file
+}
+
+button{
+  border-radius: 4px;
+  border-style: solid;
+  
+
+}
+  .btn-success{
+    color: white;
+    background-color:  #49ff01; 
+  }
 </style>
---
+++ src/views/Tech.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tech">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="This is a test of the tech page"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'tech',
+  components: {
+    HelloWorld
+  }
+}
+</script>
