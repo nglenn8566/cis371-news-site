92008a15ffafa4d35f52015fbfb57acd0cad367e
amf812 <fountaia@mail.gvsu.edu>
2018-12-04 08:36:40 -0500

    sports page works

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import ArticleView from './views/ArticleView.vue'
 import CreateView from './views/Create.vue'
 import EditArticle from './views/EditArticle.vue'
 import ArticleList from './views/ArticleList.vue'
+import Sports from './views/Sports.vue'
 
 Vue.use(Router)
 
@@ -27,6 +28,11 @@ export default new Router({
       path: '/tech',
       name: 'tech',
       component: Tech
+    },
+    {
+      path: '/sports',
+      name: 'sports',
+      component: Sports
     },
     {
       path: '/article/:uid',
